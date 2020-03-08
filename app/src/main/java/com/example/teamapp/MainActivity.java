0a4702bf660a458597923a86d887e15f7e7e822b
Gordontb <gordontb@mymail.vcu.edu>
2020-03-07 23:46:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/teamapp/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.teamapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button registerButton, loginButton;
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        loginButton =(Button)findViewById(R.id.logIn);
+        registerButton =(Button)findViewById(R.id.buttonRegister);
+        text = (TextView)findViewById(R.id.passWord);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            // Creates the on Click
+            @Override
+            public void onClick(View v) {
+                openRegistraionScreen();
+            }
+        });
+    }
+    public void openRegistraionScreen(){
+        Intent intent = new Intent (this, registrationScreen.class);
+        startActivity(intent);
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/teamapp/registrationScreen.java
@@ -0,0 +1,14 @@
+package com.example.teamapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_screen);
+    }
+}
